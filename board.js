cbac18914efff2ed8f5ef138db7df4c57a56b0c0
Dgordgun <georgdvorskii@gmail.com>
2024-11-05 21:50:31 +0200

    part of rules

---
+++ board.js
@@ -0,0 +1,8 @@
+let GameBoard = {}
+
+GameBoard.pirces = [COUNT_OF_SQUERES]
+GameBoard.side = COLORS.WHITE
+GameBoard.fiftyMove = 0
+GameBoard.hisPlay = 0
+GameBoard.play = 0
+GameBoard.isAloudToCasle = 0
