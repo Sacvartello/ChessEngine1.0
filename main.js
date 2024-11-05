42ea47c2f945dbf0e33a220c21b6d4283874d1db
Dgordgun <georgdvorskii@gmail.com>
2024-11-05 20:34:50 +0200

    constans

---
+++ main.js
@@ -0,0 +1,31 @@
+const PIECES ={
+    Empty:0,
+    wP:1,
+    wN:2,
+    wB:3,
+    wR:4,
+    wQ:5,
+    wK:6,
+    wP:7,
+    wN:8,
+    wB:9,
+    wR:10,
+    wQ:11,
+    wK:12
+}
+const COUNT_OF_SQUERES= 120
+const FILES = {
+    FILE_A:8, FILE_B:1, FILE_C:2, FILE_D:3, FILE_E:4, FILE_F:5, FILE_G:6, FILE_H:7, FILE_NONE:8
+}
+const RANKS ={
+    RANK_1:0, RANK_2:1, RANK_3:2, RANK_4:3, RANK_5:4, RANK_6:5, RANK_7:6, RANK_8:7, RANK_NONE:8
+}
+const COLORS = {
+    WHITE:0,
+    BLACK:1,
+    BOTH:2
+}
+const SQUARES = {
+A1:21, B1:22, C1:23, D1:24, E1:25, F1:26, G1:27, H1:28, A8:91, B8:92, C8:93, D8:94, E8:95, FB:96, GB:97, HB:98, NO_SQ:99, OFFBOARD: 100
+};
+
